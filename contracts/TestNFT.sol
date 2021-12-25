a8463b021d40e854f92e76187c1db47fb5de5eec
Aneesh Sachdeva <aneeshsachdeva1@gmail.com>
2021-12-24 22:45:29 -0800

    Minting now requires a function call.

--- contracts/TestNFT.sol
+++ contracts/TestNFT.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.7.0 <= 0.90;
+pragma solidity >= 0.7.0 <= 0.9.0;
 
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 import "@openzeppelin/contracts/utils/Counters.sol";
@@ -14,11 +14,11 @@ contract TestNFT is ERC721 {
     constructor(string memory name, string memory symbol) ERC721(name, symbol) { }
 
     /// Mint a token for sender.
-    function mint() external returns (uint256) {
+    function mint(address recipient) external returns (uint256) {
         _tokenIds.increment();
 
         uint256 newTokenId = _tokenIds.current();
-        _mint(msg.sender, newTokenId);
+        _mint(recipient, newTokenId);
         
         return newTokenId;
     }
--- contracts/TestToken.sol
+++ contracts/TestToken.sol
@@ -1,12 +1,11 @@
 // SPDX-License-Identifier: GPL-3.0
-pragma solidity >= 0.7.0 < 0.90;
+pragma solidity >= 0.7.0 < 0.9.0;
 
 import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
 
 contract TestToken is ERC20 {
 
     constructor(string memory name, string memory symbol) ERC20(name, symbol) {
-        _mint(msg.sender, 10000 * (10 ** 18));
     }
 
     function faucet (address recipient , uint amount) external {
