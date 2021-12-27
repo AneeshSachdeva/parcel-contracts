ffe5f1d33616e5db085da81629079119c9e3abf3
Aneesh Sachdeva <aneeshsachdeva1@gmail.com>
2021-12-26 19:32:09 -0800

    Added gas reporting to tests

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,6 +1,8 @@
+import * as dotenv from "dotenv";
 import { task } from "hardhat/config";
 import '@typechain/hardhat'
 import "@nomiclabs/hardhat-waffle";
+import "hardhat-gas-reporter"
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -15,9 +17,15 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
+dotenv.config();  // Load .env into process.env
+
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: process.env.SOLIDITY_VERSION ?? "0.8.4",
+  gasReporter: {
+    coinmarketcap: process.env.COINMARKETCAP_API_KEY,
+    currency: process.env.CURRENCY ?? 'USD'
+  }
 };
