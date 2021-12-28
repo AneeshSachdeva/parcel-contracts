fcc01146809804607994a2ae6c7ef9f1b50974bc
Aneesh Sachdeva <aneeshsachdeva1@gmail.com>
2021-12-27 20:20:55 -0800

    Implemented ParcelFactory with clones

---
+++ contracts/ParcelFactory.sol
@@ -0,0 +1,25 @@
+pragma solidity >= 0.7.0 < 0.9.0;
+
+import "./ParcelNaive.sol";
+import "@openzeppelin/contracts/proxy/Clones.sol";
+
+contract ParcelFactory {
+    address payable immutable parcelImpl;
+
+    event ParcelCreated(address _clone);
+
+    constructor() {
+        parcelImpl = payable(address(new Parcel()));
+    }
+
+    function createParcel(
+        bytes32 hashedSecret
+    ) external returns (address payable) {
+        address payable clone = payable(Clones.clone(parcelImpl));
+        Parcel(clone).initialize(hashedSecret, msg.sender);
+
+        emit ParcelCreated(clone);
+        
+        return clone;
+    }
+}
--- test/test-naive.ts
+++ test/test-naive.ts
@@ -9,9 +9,11 @@ describe("Parcel contract (naive)", function () {
   let Token: ContractFactory;
   let NFT: ContractFactory;
   let Parcel: ContractFactory;
+  let ParcelFactory: ContractFactory;
   let testToken: Contract;
   let testNFT: Contract;
   let testParcel: Contract;
+  let parcelFactory: Contract;
   let parcelKey: Buffer;
   let owner: SignerWithAddress;  // Creates the parcel and sends assets
   let addr1: SignerWithAddress;  // Sends assets to parcel
@@ -26,13 +28,15 @@ describe("Parcel contract (naive)", function () {
     Token = await ethers.getContractFactory("TestToken");
     NFT = await ethers.getContractFactory("TestNFT");
     Parcel = await ethers.getContractFactory("Parcel");
+    ParcelFactory = await ethers.getContractFactory("ParcelFactory");
     [owner, addr1, receiver] = await ethers.getSigners();
 
     // Deploy contracts.
     let hashedKey: Buffer = keccak256(parcelKey);
     testToken = await Token.deploy("TestToken", "TKN");
     testNFT = await NFT.deploy("TestNFT", "NFT");
-    testParcel = await Parcel.deploy(hashedKey);
+    testParcel = await Parcel.deploy();
+    parcelFactory = await ParcelFactory.deploy();
 
     // Load assets into wallets.
     testToken.faucet(owner.address, 100);
@@ -45,7 +49,20 @@ describe("Parcel contract (naive)", function () {
     it("Sets the right owner", async function () {
       // This test expects the sender variable stored in the contract to be
       // equal to our Signer's owner.
+      let hashedKey: Buffer = keccak256(parcelKey);
+      await testParcel.initialize(hashedKey, owner.address);
       expect(await testParcel.sender()).to.equals(owner.address);
+    });
+
+    it("Creates clone from factory", async function () {
+      const hashedKey: Buffer = keccak256(parcelKey);
+      const cloneTx = await parcelFactory.createParcel(hashedKey);
+      const { events } = await cloneTx.wait();
+      const createEvent = events.find( (e: any) => e.event == "ParcelCreated");
+      const cloneAddr = createEvent.args._clone;
+
+      let instance: Contract = new ethers.Contract(cloneAddr, Parcel.interface, owner);
+      expect(await instance.sender()).to.equals(owner.address);
     });
   })
 
@@ -83,15 +100,14 @@ describe("Parcel contract (naive)", function () {
     it("Receive assets using the correct secret", async function () {
       // By this point, parcel should be loaded with ETH, tokens, and an NFT
       // Owner locks parcel first so it's ready to be sent and received
-      let startBalance: BigNumber = await receiver.getBalance();
+      const startBalance: BigNumber = await receiver.getBalance();
       await testParcel.lock();
       await testParcel.connect(receiver).open(parcelKey);
-      let endBalance: BigNumber = await receiver.getBalance();
-      let gasCost: BigNumber = startBalance.sub(ethers.utils.parseEther("1"));
-      
-      expect(startBalance.sub(gasCost))
+      const endBalance: BigNumber = await receiver.getBalance();
+
+      expect(parseFloat(ethers.utils.formatEther(endBalance)))
         .to
-        .equals(ethers.utils.parseEther("1"));
+        .greaterThan(parseFloat(ethers.utils.formatEther(startBalance)));
       expect(await testToken.balanceOf(receiver.address)).to.equals(100);
       expect(await testNFT.ownerOf(1)).to.equals(receiver.address);
     });
