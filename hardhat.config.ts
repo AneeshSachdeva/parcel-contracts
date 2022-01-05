0523bdfa15f3c1eb8a518177d0facb209ecbfd90
Aneesh Sachdeva <aneeshsachdeva1@gmail.com>
2022-01-04 20:20:17 -0800

    Added stage-setup and balance tasks

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -2,7 +2,9 @@ import * as dotenv from "dotenv";
 import { task } from "hardhat/config";
 import '@typechain/hardhat'
 import "@nomiclabs/hardhat-waffle";
-import "hardhat-gas-reporter"
+import "hardhat-gas-reporter";
+
+dotenv.config();  // Load .env into process.env
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -14,12 +16,76 @@ task("accounts", "Prints the list of accounts", async (args, hre) => {
   }
 });
 
+task("balance", "Prints an account's balance")
+  .addParam("account", "The account's address")
+  .setAction(async (args, hre) => {
+    const account = hre.ethers.provider.getSigner(args.account);
+    const balance = await account.getBalance();
+
+    console.log(hre.ethers.utils.formatEther(balance), "ETH");
+});
+
+task(
+  "stage-setup",
+  "Prepare staging environment to run tests",
+  async (args, hre) => {
+    // Get accounts.
+    const [admin, sender, stranger, receiver] = await hre.ethers.getSigners();
+    console.log(`Admin address is ${admin.address}`);
+    
+    // Get the min balance required for each test account.
+    let minBalance: number;
+    if (process.env.STAGE_ACCOUNT_MIN_ETH_BALANCE) {
+      minBalance = parseFloat(process.env.STAGE_ACCOUNT_MIN_ETH_BALANCE);
+    } else {
+      minBalance = 0.1;
+    }
+
+    // Ensure the admin has sufficient funds.
+    let adminBalance = parseFloat(
+      hre.ethers.utils.formatEther(await admin.getBalance())
+    );
+    if (adminBalance < minBalance) {
+      throw `Admin account has insufficient funds (${adminBalance} ETH).\n`
+        + `${minBalance} ETH is required`;
+    }
+
+    // Ensure each wallet has sufficient ETH. If not, send ETH from admin.
+    for (let wallet of [sender, stranger, receiver]) {
+      let balance = parseFloat(
+        hre.ethers.utils.formatEther(await wallet.getBalance())
+      );
+
+      if (balance < minBalance) {
+        try {
+          // Send ETH from admin to wallet so that it has sufficient funds.
+          const ethValue = minBalance - balance;
+          await admin.sendTransaction({
+            to: wallet.address,
+            value: hre.ethers.utils.parseEther(`${ethValue}`)
+          })
+          console.log(`Sent ${ethValue} ETH from admin to ${wallet.address}`);
+        } catch (e) {
+          // Error sending ETH from admin to wallet.
+          console.error(e);
+          const adminBalance = hre.ethers.utils.formatEther(
+            await admin.getBalance()
+          );
+          console.log(
+            `Ensure that there are sufficient funds at address ${admin.address}`
+          );
+          console.log(`Balance: ${adminBalance} ETH`);
+          break;
+        }
+      } else {
+        console.log(`${wallet.address} has sufficient funds.`);
+      }
+    }
+});
+
 // You need to export an object to set up your config
 // Go to https://hardhat.org/config/ to learn more
 
-dotenv.config();  // Load .env into process.env
-console.log(process.env.ALCHEMY_API_KEY);
-console.log(process.env.MNEMONIC);
 /**
  * @type import('hardhat/config').HardhatUserConfig
  */
